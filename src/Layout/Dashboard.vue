ef69c7f69f1a7b806d47478d6072034439b17516
goel1443 <goel1443@gmail.com>
2023-08-20 08:16:11 +0530

    today 20-08-2023

--- src/Layout/Dashboard.vue
+++ src/Layout/Dashboard.vue
@@ -3,32 +3,27 @@
     <section class="section">
       <div class="custom-container">
         <div class="dashboard-row">
-          <div class="dashboard-navbar ">
-
-            <div class="header fixed-top ">
-              <NavBar :sideclose="sideclose"    @opensidebar="callTogglesnavbar" />
+          <div class="dashboard-navbar">
+            <div class="header fixed-top">
+              <NavBar :sideclose="sideclose" @opensidebar="callTogglesnavbar" />
             </div>
           </div>
-       
-          <div class="dashboard-sidebar" :class="{active:sideclose }"  id="sidebarMenu">
-           
-  
-          <SideBar @closesidebar="callTogglesidebar" />
-        
-          
-          </div> 
-       
-          <div class="dashboard-content" :class="{active:sideclose }">
-              <slot>
-               
-              </slot>
+
+          <div
+            class="dashboard-sidebar"
+            :class="{ active: sideclose }"
+            id="sidebarMenu"
+          >
+            <SideBar @closesidebar="callTogglesidebar" />
           </div>
 
-          <div class="dashboard-footer " :class="{active:sideclose }">
-             <Footer />
+          <div class="dashboard-content" :class="{ active: sideclose }">
+            <slot> </slot>
           </div>
 
-
+          <div class="dashboard-footer" :class="{ active: sideclose }">
+            <Footer />
+          </div>
         </div>
       </div>
     </section>
@@ -36,67 +31,48 @@
 </template>
 
 <script>
-import Footer from '@/components/Footer'
-import NavBar from '@/components/NavBar';
-import SideBar from '@/components/SideBar';
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import SideBar from "@/components/SideBar";
 export default {
-  name:'Dashboard',
-  components:{
+  name: "Dashboard",
+  components: {
     NavBar,
     SideBar,
-    Footer
+    Footer,
   },
 
   data() {
     return {
       sideclose: false,
-      infoBar: false
-      
-    }
+      infoBar: false,
+    };
   },
 
   methods: {
-
     callTogglesidebar(value) {
-      if(this.sideclose==false)
-      {
-      this.sideclose = !value
-    
-  
-
+      if (this.sideclose == false) {
+        this.sideclose = !value;
+      } else {
+        this.sideclose = value;
       }
-      else{
-
-        this.sideclose = value
-      }
-      
     },
 
     callTogglesnavbar(value) {
-      this.sideclose = value
-      document.getElementById("sidebarMenu").style="display:block"
+      this.sideclose = value;
+      document.getElementById("sidebarMenu").style = "display:block";
     },
-
-
-   
-
-   
-  }
-
-}
+  },
+};
 </script>
 
 <style scoped>
-
-
-.section{
-
+.section {
   background-color: #1d1930;
 }
-.dashboard-row{
+.dashboard-row {
   min-height: 100%;
 }
-
 
 .dashboard-sidebar {
   width: 250px;
@@ -105,140 +81,109 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
-  
- z-index: 999;
-  height:100%;
+
+  z-index: 999;
+  height: 100%;
 }
 
-
-
-
-.dashboard-content{
-
-  margin-left:250px;
-  margin-top:80px;
-   min-height: 100vh;
-   color:var(--white);
-   font-family:'Roboto';
-  z-index:-1;
+.dashboard-content {
+  margin-left: 250px;
+  margin-top: 80px;
+  min-height: 100vh;
+  color: var(--white);
+  font-family: "Roboto";
+  z-index: -1;
   background-color: #151122;
-  transition: 0.5s  ;
-  padding:0px 15px 0px 15px;
- 
+  transition: 0.5s;
+  padding: 0px 15px 0px 15px;
 }
 
-
-.dashboard-footer{
-
-  margin-left:250px;
-  transition: 0.5s  ;
+.dashboard-footer {
+  margin-left: 250px;
+  transition: 0.5s;
   background-color: #1d1930;
-  color:var(--white);
-   font-family:'Roboto';
-  
+  color: var(--white);
+  font-family: "Roboto";
 }
-
 
 .dashboard-content.active,
-.dashboard-footer.active{
-
-  margin-left:0px;
+.dashboard-footer.active {
+  margin-left: 0px;
 }
 
 .header {
-    background-color: #1d1930;
-    height: 80px;
-    color: var(--white);
-    font-family: "Roboto";
-    font-size: 14px;
-    z-index: 3;
-    
+  background-color: #1d1930;
+  height: 80px;
+  color: var(--white);
+  font-family: "Roboto";
+  font-size: 14px;
+  z-index: 3;
+}
+
+@media all and (min-width: 992px) and (max-width: 1199px) {
+  #sidebarMenu {
+    display: none;
   }
 
-
-@media all and (min-width:992px) and (max-width: 1199px) {
-
-  #sidebarMenu{
-
-    display:none;
-  }
-
-  .dashboard-sidebar{
-
-    transition: 0.5s  ;
-  margin-left: 0px;
+  .dashboard-sidebar {
+    transition: 0.5s;
+    margin-left: 0px;
   }
 
   .dashboard-sidebar.active {
-    transition: 0.5s ;
-  margin-left: -250px;
+    transition: 0.5s;
+    margin-left: -250px;
+  }
+
+  .dashboard-content.active,
+  .dashboard-content,
+  .dashboard-footer {
+    margin-left: 0px;
+  }
 }
 
-.dashboard-content.active,
-.dashboard-content,
-.dashboard-footer
-{
+@media all and (min-width: 768px) and (max-width: 991px) {
+  #sidebarMenu {
+    display: none;
+  }
 
-margin-left:0px;
+  .dashboard-sidebar {
+    transition: 0.5s;
+    margin-left: 0px;
+  }
+  .dashboard-sidebar.active {
+    transition: 0.5s;
+    margin-left: -250px;
+  }
+
+  .dashboard-content.active,
+  .dashboard-content,
+  .dashboard-footer {
+    margin-left: 0px;
+  }
 }
 
-}
+@media all and (min-width: 320px) and (max-width: 767px) {
+  #sidebarMenu {
+    display: none;
+  }
 
+  .dashboard-sidebar {
+    transition: 0.5s;
+    margin-left: 0px;
+  }
+  .dashboard-sidebar.active {
+    transition: 0.5s;
+    margin-left: -250px;
+  }
 
-@media all and (min-width:768px) and (max-width: 991px) {
-
-#sidebarMenu{
-
-  display:none;
-}
-
-.dashboard-sidebar{
-
-  transition: 0.5s ;
-margin-left: 0px;
-}
-.dashboard-sidebar.active {
-  transition: 0.5s ;
-  margin-left: -250px;
-}
-
-.dashboard-content.active,
-.dashboard-content,
-.dashboard-footer{
-
-margin-left:0px;
-}
-
-}
-
-
-@media all and (min-width:320px) and (max-width:767px) {
-
-#sidebarMenu{
-
-  display:none;
-}
-
-
-.dashboard-sidebar{
-
-transition: 0.5s ;
-margin-left: 0px;
-}
-.dashboard-sidebar.active {
-  transition: 0.5s ;
-  margin-left: -250px;
-}
-
-.dashboard-content.active,
-.dashboard-content,
-.dashboard-footer{
-
-margin-left:0px;
-}
-.dashboard-content{
-
-  padding:0px !important;
-}
+  .dashboard-content.active,
+  .dashboard-content,
+  .dashboard-footer {
+    margin-left: 0px;
+  }
+  .dashboard-content {
+    padding: 0px !important;
+  }
 }
 </style>
