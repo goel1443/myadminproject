0fee40391d041bb8536b14329ad06c1b44ac2d9f
goel1443 <goel1443@gmail.com>
2024-12-27 17:24:58 +0530

    updated code commit 27-12-2024

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap';
-import VueApexCharts from "vue3-apexcharts";
+// import VueApexCharts from "vue3-apexcharts";
 import Vue3Autocounter from 'vue3-autocounter';
 import DropZone from 'dropzone-vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pagination from 'v-pagination-3';
-createApp(App).use(store).use(router).component('pagination', Pagination).use(VueApexCharts).component('vue3-autocounter', Vue3Autocounter).use(DropZone).mount('#app')
+createApp(App).use(store).use(router).component('pagination', Pagination).component('vue3-autocounter', Vue3Autocounter).use(DropZone).mount('#app')
